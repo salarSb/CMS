baf6fa562d67cae543617828b790196d3f5f7234
salar <salarsabeti88@gmail.com>
2021-11-12 16:16:58 +0330

    adding module admin and routes for AdminController.php, AdminPostController.php and AdminUserController.php

---
+++ Modules/Admin/Http/Controllers/AdminPostController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Modules\Admin\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class AdminPostController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
