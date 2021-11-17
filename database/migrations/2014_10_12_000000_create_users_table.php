fd71fdb873d2cd67966fba401a0b00f83c122eab
salar <salarsabeti88@gmail.com>
2021-11-17 12:56:22 +0330

    changing users table and adding CheckAdmin.php middleware for admin routes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('level')->default('user');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('family')->nullable();
+            $table->string('mobile')->unique();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
