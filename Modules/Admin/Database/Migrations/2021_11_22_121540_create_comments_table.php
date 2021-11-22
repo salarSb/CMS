9dc74524a44e789f5173df09697585185f1855bb
salar <salarsabeti88@gmail.com>
2021-11-22 16:19:33 +0330

    adding Tag model and its polymorphic manyToMany relation with Post model. also adding Comment model and its polymorphic oneToMany relation with Post

---
+++ Modules/Admin/Database/Migrations/2021_11_22_121540_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('text');
+            $table->boolean('approved')->default(0);
+            $table->morphs('commentable');
+            $table->foreign('parent_id')->references('id')->on('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
---
+++ app/Models/Comment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function isApproved()
+    {
+        return $this->approved;
+    }
+}
